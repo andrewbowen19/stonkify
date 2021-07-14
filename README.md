ac09878bbf809d854debb2d5466f03b53884921f
andrewbowen19 <atbowen@gmail.com>
2021-07-13 23:17:52 -0400

    Added general news to sentiment script and updated README

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 Sandbox python web app for stock data visualization
 
+This applet utilizes both the [pandas data reader](https://pandas-datareader.readthedocs.io/en/latest/) and [Finnhub API](https://finnhub.io/dashboard). The dashboard was written using [Dash](https://dash.plotly.com) & [Plotly](https://plotly.com).
 
+Running `python stonks.py` from the command line should set up the local server for our dashboard. You can then enter the local address into a browser to view the dashboard. From there, you can enter the ticker symbol of any stock into the search field to view
 
+Company news sentinemnt provided by [Finnhub](https://finnhub.io/docs/api/company-news) coming soon!
 
+ 
 
--- sentiment.py
+++ sentiment.py
@@ -12,23 +12,60 @@ import os
 # Get free finnhub key from here: https://finnhub.io/dashboard
 finnhub_key = os.environ['FINNHUB_KEY']
 
-def general_news_sentiment():
+def market_news(category='general'):
     '''
     Gets general market sentiment analysis
+    category: str, can be one of general, crypto, forex, merger
     '''
     print('Getting market news...')
+    r = requests.get(f'https://finnhub.io/api/v1/news?category={category}?&token={finnhub_key}')
+    print(r.json())
     
     
 def company_news_sentiment(symbol):
     '''
     Function to return company news senntiment from social media
+    symbol: str, company ticker symbol (ex. AAPL)
     '''
     print(f'Getting sentiment analysis for {symbol}')
-    r = requests.get(f'https://finnhub.io/api/v1/news-sentiment?symbol=DIS&token={finnhub_key}')
+    r = requests.get(f'https://finnhub.io/api/v1/news-sentiment?symbol={symbol}&token={finnhub_key}')
     
 #    May need to re-factor this dataframe
     df = pd.DataFrame.from_dict(r.json())
     print(df)
+    
+def get_company_news(symbol, start, end):
+    '''
+    returns all recent headlines centered on the company
+    start, end: str, date interval of news request YYYY-MM-DD
+    symbol: str, company ticker symbol (ex. AAPL)
+    '''
+    print('Fetching company news')
+    r = requests.get(f'https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2021-03-01&to=2021-03-09&token=c17ck1f48v6se55vrra0')
+    
+    print(r.json())
+    
+    
+    
+
 
 if __name__ == "__main__":
     company_news_sentiment('AMZN')
+#    get_company_news('AAPL', '2021-01-04', '2021-07-13')
+    market_news()
+
+
+# TODO:
+# Could make this^ one function - change out strings
+# One API response function, call with diff request strings
+# Add more Finnhub API features!
+
+
+#def api_request(params):
+#    res = requests(string + params + token)
+#    print(res.json())
+#
+#    convert to df/numpy array
+
+
+
